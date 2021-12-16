3638f59e9b19559072903d794f0791d28b9ab083
sanjeevkumar-kiwi <sanjeev.kumar@kiwitech.com>
2021-12-17 00:43:04 +0530

    assigned name

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -195,7 +195,7 @@ class ProjectController extends Controller
             $projects = Project::all();
 
             $projects->transform(function($project){
-                $project->assigned_user = $project->users()->select(['id','first_name','last_name'])->get();
+                $project->assigned_user = $project->users;
                 return $project;
             });
 
