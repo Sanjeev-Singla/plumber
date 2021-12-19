03976a04b0103abe40bfd25285b5b78025aa4a81
sanjeevkumar-kiwi <sanjeev.kumar@kiwitech.com>
2021-12-19 14:11:55 +0530

    added users

--- app/Http/Controllers/Admin/VehicleController.php
+++ app/Http/Controllers/Admin/VehicleController.php
@@ -146,7 +146,8 @@ class VehicleController extends ApiBaseController
     public function allVehicle(){
         $admin = Auth::user();
 		if( $admin['role'] == 0 || $admin['role'] == 1){
-            $vehicle = Vehicle::where('status',\Config::get('constant.vehicles.status.enabled'))
+            $vehicle = Vehicle::with('allotedUser')
+                        ->where('status',\Config::get('constant.vehicles.status.enabled'))
                         ->get();
 
             return $this->sendResponse($vehicle,'Vehicle List',200,200);
