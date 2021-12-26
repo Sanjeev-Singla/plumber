0d0cd3e20ed884681bae6e0cf42088184b5ab699
sanjeevkumar-kiwi <sanjeev.kumar@kiwitech.com>
2021-12-26 23:46:27 +0530

    filter on request list

--- app/Http/Controllers/Admin/MaterialController.php
+++ app/Http/Controllers/Admin/MaterialController.php
@@ -74,7 +74,7 @@ class MaterialController extends ApiBaseController
             ->orderBy('id','DESC')
             ->get();
 
-            if (!blank($request->employee_id)) {
+            if (!blank($request->employee_id)) {dd();
                 $materials = $materials->where('request_user_id	',$request->employee_id);
             }elseif(!blank($request->date_from) &&  !blank($request->date_to)){
                 $materials = $materials->whereBetwwen('created_at',[$request->date_from,$request->date_to]);
